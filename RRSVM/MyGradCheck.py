all test passes! TODO: add features on Max and Average Pooling Fixing the s and set them to specific parameter

RRSVM/MyGradCheck.py
@@ -61,8 +61,8 @@ def get_numerical_jacobian(fn, input, target, eps=1e-3):
     x_tensors = [t for t in iter_tensors(target, True)]
     j_tensors = [t for t in iter_tensors(jacobian)]
 
-    outa = torch.DoubleTensor(output_size)
-    outb = torch.DoubleTensor(output_size)
+    outa = torch.FloatTensor(output_size)
+    outb = torch.FloatTensor(output_size)
 
     # TODO: compare structure
     for x_tensor, d_tensor in zip(x_tensors, j_tensors):
@@ -75,8 +75,9 @@ def get_numerical_jacobian(fn, input, target, eps=1e-3):
             outb.copy_(fn(input))
             flat_tensor[i] = orig
 
-            outb.add_(-1, outa).div_(2 * eps)
-            d_tensor[i] = outb
+            grad = (outb.numpy() - outa.numpy())/ (2*eps)
+            # outb.add_(-1, outa).div_(2 * eps)
+            d_tensor[i] = torch.from_numpy(grad)
 
     return jacobian
 
@@ -139,16 +140,18 @@ def gradcheck(func, inputs, eps=1e-6, atol=1e-5, rtol=1e-3):
 
         def fn(input):
             return _as_tuple(func(*input))[i].data
-
-        numerical = get_numerical_jacobian(fn, inputs, inputs, eps)
         analytical = get_analytical_jacobian(_as_tuple(inputs), o)
-
-        for a, n in zip(analytical, numerical):
+        print analytical
+        numerical = get_numerical_jacobian(fn, inputs, inputs, eps)
+        print numerical
+        for k,  (a, n) in enumerate(zip(analytical, numerical)):
+            relative_loss = (a - n) / (n + eps)
+            print relative_loss
             if not ((a - n).abs() <= (atol + rtol * n.abs())).all():
-                print "{:d}th Input Grad is problematic".format(i)
-                Flag = False
-            else:
-                print "{:d}th Input Grad is None problematic".format(i)
+                 print "{:d}th Input Grad is problematic".format(i)
+                 Flag = False
+            # else:
+            #     print "{:d}th Input Grad is None problematic".format(i)
 
 
     # check if the backward multiplies by grad_output

RRSVM/RRSVM.py
@@ -18,10 +18,10 @@ class RRSVM_F(torch.autograd.Function):
 
     def forward(self, input, s):
 
-        assert self.kernel_size == s.size(1) and self.kernel_size == s.size(2), 'Kernel size should be the same as s size'
+        assert self.kernel_size * self.kernel_size == s.size(1), 'Kernel size should be the same as s size'
         k_s = s.dim()
-        if k_s != 3:
-            raise RuntimeError('S should be 3D [Channel, kH, kW]')
+        if k_s != 2:
+            raise RuntimeError('S should be 2D [Channel, kH * kW]')
 
         k_i = input.dim()
         if k_i != 4:
@@ -128,7 +128,7 @@ class RRSVM_Module(torch.nn.Module):
         self.stride = stride
         self.padding = padding
         self.dilation = dilation
-        self.s = Parameter(torch.Tensor(in_channels, self.kernel_size, self.kernel_size))
+        self.s = Parameter(torch.Tensor(in_channels, self.kernel_size * self.kernel_size))
         # initialize s:
         n_elt = in_channels * self.kernel_size * self.kernel_size
         init_val = 1. / n_elt

RRSVM/Test.py
@@ -9,28 +9,45 @@ import numpy as np
 # TODO: May be you need the S to be 2D ...
 # TODO: Think about padding case with zero
 
-def gradTest():
-    input = (Variable(torch.FloatTensor(torch.randn(1, 1, 3, 3)), requires_grad=True),
-             Variable(torch.FloatTensor(torch.randn(1, 3, 3)), requires_grad=True),)
 
-    F = RRSVM.RRSVM_F(kernel_size=3, padding=0, stride=1, dilation=1)
+def test_gradient(input, kernel_size=3, padding=0, stride=1):
 
-    test = gradcheck(lambda i, s: F(i, s), inputs=input, eps=1e-6, atol=1e-4)
+    F = RRSVM.RRSVM_F(kernel_size=kernel_size, padding=padding, stride=stride, dilation=1)
+
+    test = gradcheck(lambda i, s: F(i, s), inputs=input, eps=1e-3, atol=1e-2, rtol=1e-1)
     print test
 
-def TestOutput():
-    input = (Variable(torch.FloatTensor(torch.randn(1, 1, 3, 3)), requires_grad=True),
-             Variable(torch.FloatTensor(torch.randn(1, 3, 3)), requires_grad=True),)
 
-    F = RRSVM.RRSVM_F(kernel_size=3, padding=0, stride=1, dilation=1)
-    analytical, _ = F(*input)
-    numerical, _ = get_numerical_output(*input, kernel_size=3, padding=0, stride=1, dilation=1)
+def test_forward(input, kernel_size=3, padding=1, stride=2, dilation=1):
+    # input = (Variable(torch.FloatTensor(torch.randn(1, 1, 5, 5)), requires_grad=True),
+    #          Variable(torch.FloatTensor(torch.randn(1, 9)), requires_grad=True),)
+
+    F = RRSVM.RRSVM_F(kernel_size, padding, stride, dilation=1)
+    analytical, analytical_indices = F(*input)
+    analytical = analytical.data.numpy()
+    analytical_indices = analytical_indices.data.numpy()
+    numerical, numerical_indices = get_numerical_output(*input, kernel_size=kernel_size, padding=padding, stride=stride, dilation=1)
+
+    atol = 1e-5
+    rtol = 1e-3
+    if not (np.absolute(numerical - analytical) <= (atol + rtol * np.absolute(numerical))).all():
+        print "Output Failed Foward Test"
+    else:
+        print "Ouput Pass Foward Test"
+
+    if not (np.absolute(numerical - analytical) <= (atol + rtol * np.absolute(numerical))).all():
+        print "Indices Failed Foward Test"
+    else:
+        print "Indices Pass Foward Test"
     # test = gradcheck(lambda i, s: F(i, s), inputs=input, eps=1e-6, atol=1e-4)
-    print "DONE"
+    # print "DONE"
+
 
 def get_numerical_output(input, s, kernel_size=3, padding=0, stride=1, dilation=1):
-    input = input.data.numpy()
-    s = s.data.numpy()
+
+    if isinstance(input, (Variable, torch.Tensor)):
+        input = input.data.numpy()
+        s = s.data.numpy()
 
     input_n = input.shape[0]
     input_d = input.shape[1]
@@ -40,39 +57,61 @@ def get_numerical_output(input, s, kernel_size=3, padding=0, stride=1, dilation=
     output_h = (input_h+ 2 * padding - (dilation * (kernel_size - 1) + 1)) / stride + 1
     output_w = (input_w+ 2 * padding - (dilation * (kernel_size - 1) + 1)) / stride + 1
 
-
     output_size = [input_n, input_d, output_h, output_w]
-    output = np.zeros(output_size)
+    output = np.zeros(output_size, dtype=np.float32)
     output_indices_size = [input_n, input_d, output_h, output_w, kernel_size*kernel_size]
-    output_indices = np.zeros(output_indices_size)
+    output_indices = np.zeros(output_indices_size, dtype=np.long)
 
     for idx_n in range(input_n):
         n_input = input[idx_n]
         for idx_d in range(input_d):
             d_s = s[idx_d]
-            d_s_flat = d_s.flatten()
+            # d_s_flat = d_s.flatten()
             n_d_input = n_input[idx_d]
+            n_d_input = pad2d(n_d_input, padding)
             for idx_h in range(output_h):
                 for idx_w in range(output_w):
                     elements = np.zeros([kernel_size, kernel_size])
                     # indices = np.zeros(kernel_size*kernel_size)
+
                     for i in range(kernel_size):
                         for j in range(kernel_size):
-                            elements[i, j] = n_d_input[idx_h+i, idx_w+j]
+                            elements[i, j] = n_d_input[idx_h*stride+i, idx_w*stride+j]
 
                     elements = elements.flatten()
                     sorted_elements = -np.sort(-elements)
                     sorted_indices = np.argsort(-elements)
                     output_indices[idx_n, idx_d, idx_h, idx_w] = sorted_indices
-                    output[idx_n, idx_d, idx_h, idx_w] = np.sum(np.multiply(sorted_elements, d_s_flat))
-                    #FIXMe: Not finished yet
+                    output[idx_n, idx_d, idx_h, idx_w] = np.sum(np.multiply(sorted_elements, d_s))
+
+    return output, output_indices
 
 
 
+# def get_numerical_gradInput():
 
-    print "DB"
 
+
+def pad2d(array2d, padding):
+    if padding == 0:
+        return array2d
+
+    array_shape = array2d.shape
+    if len(array_shape) != 2 :
+        raise NotImplementedError('Only Supports 2D padding')
+    new_array = np.zeros([array_shape[0]+ 2*padding, array_shape[1] + 2* padding])
+    new_array_shape = new_array.shape
+    new_array[padding:new_array_shape[0]-padding, padding:new_array_shape[1]-padding] = array2d
+    return new_array
 
 if __name__ == '__main__':
-    gradTest()
-    # TestOutput()
+    # test_gradient()
+    input = (Variable(torch.FloatTensor(torch.randn(1, 1, 6, 6)), requires_grad=True),
+             Variable(torch.FloatTensor(torch.randn(1, 9)), requires_grad=True),)
+    # test_gradient(input)
+    output, output_indices = get_numerical_output(*input,kernel_size=3, padding=0, stride=3)
+    print "Input\n"
+    print input
+    print 'Output\n'
+    print output
+    test_gradient(input, kernel_size=3, padding=0, stride=3)