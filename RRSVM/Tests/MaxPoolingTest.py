8d2a5ab9a99020b4e6670012c5dcca1432624130
zijunwei <zijwei@cs.stonybrook.edu>
2017-09-12 14:35:18 -0400

    extremely slow

---
+++ RRSVM/Tests/MaxPoolingTest.py
@@ -0,0 +1,18 @@
+# This is a test among maxpooling, convolution and RRSVM
+
+from torch.autograd import Variable
+import torch
+from torch.autograd import gradcheck
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+if __name__ == '__main__':
+    kernel_size = 2
+    n_channel = 100
+    feature_size = 6
+    batch_size = 3
+    input = (Variable(torch.FloatTensor(torch.randn(1, n_channel, feature_size, feature_size)), requires_grad=True), kernel_size)
+    f_max_pooling = F.max_pool2d
+    print gradcheck(f_max_pooling, input, eps=1e-3)
--- RRSVM/Tests/RRSVM_Test.py
+++ RRSVM/Tests/RRSVM_Test.py
@@ -1,18 +1,18 @@
 import torchvision.models
-import RRSVM
+import RRSVM.RRSVM.RRSVM
 from torch.autograd import Variable
 import torch
 # from torch.autograd import gradcheck
-from MyGradCheck import gradcheck
+from RRSVM.RRSVM.MyGradCheck import gradcheck
 import numpy as np
-import sys
 
-
+# TODO: May be you need the S to be 2D ...
+# TODO: Think about padding case with zero
 
 
 def test_gradient(input, kernel_size=3, padding=0, stride=1):
 
-    F = RRSVM.RRSVM_F(kernel_size=kernel_size, padding=padding, stride=stride, dilation=1)
+    F = RRSVM.RRSVM.RRSVM_F(kernel_size=kernel_size, padding=padding, stride=stride, dilation=1)
 
     test = gradcheck(lambda i, s: F(i, s), inputs=input, eps=1e-3, atol=1e-3, rtol=1e-3)
     if test == True:
@@ -25,33 +25,20 @@ def test_forward(input, kernel_size=3, padding=1, stride=2, dilation=1):
     # input = (Variable(torch.FloatTensor(torch.randn(1, 1, 5, 5)), requires_grad=True),
     #          Variable(torch.FloatTensor(torch.randn(1, 9)), requires_grad=True),)
 
-    F = RRSVM.RRSVM_F(kernel_size, padding, stride, dilation=1)
-
-    numerical, numerical_indices = get_numerical_output(*input, kernel_size=kernel_size, padding=padding, stride=stride, dilation=1)
-
-    if torch.cuda.is_available():
-        input = [i.cuda() for i in input]
-    else:
-        print("Cuda device not detected on this device")
-        sys.exit(-1)
+    F = RRSVM.RRSVM.RRSVM_F(kernel_size, padding, stride, dilation=1)
     analytical, analytical_indices = F(*input)
-    analytical = analytical.cpu().data.numpy()
-    analytical_indices = analytical_indices.cpu().data.numpy()
+    analytical = analytical.data.numpy()
+    analytical_indices = analytical_indices.data.numpy()
+    numerical, numerical_indices = get_numerical_output(*input, kernel_size=kernel_size, padding=padding, stride=stride, dilation=1)
 
     atol = 1e-5
     rtol = 1e-3
-
-
     if not (np.absolute(numerical - analytical) <= (atol + rtol * np.absolute(numerical))).all():
         print "Output Failed Foward Test"
     else:
         print "Ouput Pass Foward Test"
 
-    relative_loss = (numerical - analytical) / (numerical + 1e-6)
-    print "Max Diff: {:.04f}".format((np.abs(relative_loss).max()))
-
-
-    if not (np.absolute(numerical_indices - analytical_indices) <= (atol + rtol * np.absolute(numerical_indices))).all():
+    if not (np.absolute(numerical - analytical) <= (atol + rtol * np.absolute(numerical))).all():
         print "Indices Failed Foward Test"
     else:
         print "Indices Pass Foward Test"
@@ -122,11 +109,17 @@ def pad2d(array2d, padding):
 
 if __name__ == '__main__':
     # test_gradient()
-    kernel_size = 2
-    n_channel = 10
+    kernel_size = 3
+    n_channel = 3
     feature_size = 20
     input = (Variable(torch.FloatTensor(torch.randn(1, n_channel, feature_size, feature_size)), requires_grad=True),
              Variable(torch.FloatTensor(torch.randn(n_channel, kernel_size**2)), requires_grad=True),)
-
+    # test_gradient(input)
+    # test_forward(input, kernel_size=3, padding=0, stride=3)
+    # output, output_indices = get_numerical_output(*input,kernel_size=3, padding=0, stride=3)
+    # print "Input\n"
+    # print input
+    # print 'Output\n'
+    # print output
     test_forward(input, kernel_size=kernel_size, padding=0, stride=kernel_size, dilation=1)
     test_gradient(input, kernel_size=kernel_size, padding=0, stride=kernel_size)
