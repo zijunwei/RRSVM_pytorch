e0d66c854fbf3340d0c8a3c9245b10c40e5d0ec8
Minh Hoai <minhhoai@cs.stonybrook.edu>
2018-01-05 11:43:55 -0500

    Commit again

--- RRSVM/Tests/RRSVM_Test.py
+++ RRSVM/Tests/RRSVM_Test.py
@@ -140,20 +140,24 @@ def test1(case_id):
 # use random non-negative inputs
 def test2(case_id):
     if case_id == 1:
+        n_im = 1
         kernel_size = 2
         n_channel = 1
         feature_size = 2
     elif case_id == 2:
+        n_im = 2
         kernel_size = 2
         n_channel = 3
         feature_size = 7
     elif case_id == 3:
+        n_im = 3
         kernel_size = 5
         n_channel = 10
         feature_size = 14
 
-    A = torch.randn(1, n_channel, feature_size, feature_size)
-    A[A < 0] = 0.0
+    A = torch.randperm(n_im*n_channel*feature_size*feature_size).float()
+    A = A.view(n_im, n_channel, feature_size, feature_size)
+
     input = (Variable(torch.FloatTensor(A), requires_grad=True),
              Variable(torch.FloatTensor(torch.randn(n_channel, kernel_size ** 2)), requires_grad=True),)
     test_forward(input, kernel_size=kernel_size, padding=0, stride=kernel_size, dilation=1)
@@ -161,9 +165,9 @@ def test2(case_id):
 
 
 if __name__ == '__main__':
-    for ii in range(5):
-        print("---- Test 1, Case {}".format(ii+1))
-        test1(ii+1)
+    # for ii in range(5):
+    #     print("---- Test 1, Case {}".format(ii+1))
+    #     test1(ii+1)
 
     for ii in range(3):
         print("---- Test 2, Case {}".format(ii+1))
